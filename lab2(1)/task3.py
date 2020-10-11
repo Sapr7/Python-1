40f1e16ee1603df17cbdf4220d982cd453ce293d
Sapr7 <sapronov.iuf@phystech.edu>
2020-10-12 02:07:29 +0300

    Never late

---
+++ lab2(1)/task3.py
@@ -0,0 +1,10 @@
+import turtle as t
+
+t.shape('arrow')
+t.forward(50)
+t.left(90)
+t.forward(50)
+t.left(90)
+t.forward(50)
+t.left(90)
+t.forward(50)
---
+++ lab2(1)/task5.py
@@ -0,0 +1,21 @@
+import turtle as t
+def cherepaha(n):
+	for n in range(i):
+		t.forward(10+n*20)
+		t.left(90)
+		t.forward(10+n*20)
+		t.left(90)
+		t.forward(10+n*20)
+		t.left(90)
+		t.forward(10+n*20)
+		t.left(90)
+		t.forward(10+n*20)
+		t.penup()
+		t.left(180)
+		t.forward(20+n*20)
+		t.left(90)
+		t.forward(10)
+		t.left(90)
+		t.pendown()
+i=10
+cherepaha(i)
